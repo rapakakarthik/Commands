00debff71585f1b2b71936d3a04a3a8bbf17d975
Rapaka Karthik <rapakakarthik7@gmail.com>
2025-07-26 20:20:27 +0530

    added commands in linux file

--- linux-commands.sh
+++ linux-commands.sh
@@ -4,4 +4,12 @@ dpkg --list
 dpkg --list zip 
 -> To check the specfic pacakge is listed on linux 
 
-lsb_release -an
+
+To check the instance details or version of the instance 
+lsb_release -a
+
+
+To connect to the remote instances or server use this command
+
+go to the pem key path this pem key shloud have write permissions
+ssh -i linux_key.pem ubuntu@13.201.5.39 
