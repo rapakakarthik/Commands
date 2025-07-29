c1539c788d1b67087dd922a8ffce0ed9854e315a
Rapaka Karthik <rapakakarthik7@gmail.com>
2025-07-29 17:44:47 +0530

    added commands

--- linux-commands.sh
+++ linux-commands.sh
@@ -9,7 +9,10 @@ To check the instance details or version of the instance
 lsb_release -a
 
 
-To connect to the remote instances or server use this command
+###To connect to the remote instances or server use this command
 
 go to the pem key path this pem key shloud have write permissions
 ssh -i linux_key.pem ubuntu@13.201.5.39 
+
+# To chanage the hostname on server use this command
+sudo hostnamectl set-hostname <name>
