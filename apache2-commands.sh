67d0d830b33636ff1268a1fe94ce5c1b4ff22326
Rapaka Karthik <rapakakarthik7@gmail.com>
2025-07-29 13:53:43 +0530

    added virtual host configuration details on apache2 file

---
+++ apache2-commands.sh
@@ -0,0 +1,26 @@
+1. apachectl configtest
+1. apachectl configtest
+--> To check the apache virtual host configuration syntax is correct or not  
+
+
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install apache2 -y
+sudo systemctl status apache2
+sudo systemctl start apache2
+sudo systemctl enable apache2
+Create virtual host configuration file on this path - this file will use to manage multiple sites to maintain seperate configuration on apache2 server
+/etc/apache2/sites-available
+touch qa-b2b-api.conf
+--> add info in this file 
+
+
+sudo a2dissite 000-default.conf
+sudo a2ensite qa-b2b-api.conf
+sudo systemctl reload apache2
+
+sudo a2enmod rewrite
+sudo systemctl restart apache2
+
+sudo a2enmod headers
+sudo systemctl restart apache2
