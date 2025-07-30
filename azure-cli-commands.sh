31c4631ba78b936dd9d94a282819e3e0228180eb
Rapaka Karthik <rapakakarthik7@gmail.com>
2025-07-30 11:47:47 +0530

    created azure-cli file and added the commands

---
+++ azure-cli-commands.sh
@@ -0,0 +1,7 @@
+To login to the azure via terminal (cmd)
+--> az login
+
+
+in azure cli use this command to create the Resource Group - login in to the azure and check this resource group is created or not 
+az group create --name rg-karthik-cli --location centralindia
+
