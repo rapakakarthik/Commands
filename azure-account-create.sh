bb2a6201e49ae2a2ae8626121bd0cf684009bfa1
Rapaka Karthik <rapakakarthik7@gmail.com>
2025-07-30 11:29:50 +0530

    azure-account-creation process file added

---
+++ azure-account-create.sh
@@ -0,0 +1,10 @@
+Open Portal on browser
+https://portal.azure.com
+
+create microsoft account - after creating account it will redirect to the azure portal there loign with this credentials 
+
+you will see the dashboard with subsctiptions details 
+Start with an Azure free trial - click on start - you will see 2 options [Try Azure for free] [Pay as yo go]
+
+click on try azure for free - it will redicrect to the azure portal it will ask to fill the details of verification of user details with credit card or debit card details 
+and 2 rupess will be debited form account ans will refunded again to same account.
