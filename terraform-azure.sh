2200a569abe9baa503464f8ea53bf3f7f1fa92c2
Rapaka Karthik <rapakakarthik7@gmail.com>
2025-08-01 18:53:01 +0530

    created terrafom-azure file and added commands in both terraform files

--- terraform-azure.sh
+++ terraform-azure.sh
@@ -1,4 +1,4 @@
-terraform install CLI from browser it will install the zip folder copy that zip to the c drive and create terrraform folder and paste into it 
+terraform install CLI form browser it will install the zip folder copy that zip to the c drive and create terrraform folder and paste into it 
 and there unzip the terrafom folder there you can see the terraform.exe file 
 this file need to copy into the environment variable path - means we are tell the windows system that our terraform.exe is in this folder or path 
 so any time you run it will go this folder 
@@ -16,10 +16,17 @@ Path will be like this : C:\Terraform\terraform_1.12.2_windows_amd64
 open the command prompt and type terraform version
 it will show the version of the terraform
 terraform -v
-
 #################################################################################
 
 if we want to work with terraform with cloud platform open the vs code terminal select the gitbash terminal and 
 first need to tell the terraform about the cloud platform we are using 
 
-command for AWS is : aws configure
+command for AWS is : aws login
+
+
+
+
+
+#################################################################################
+
+
